32f2da5b4940e85940ea5f12c8d7b296c863c0ad
Emanuele Olivetti <olivetti@fbk.eu>
2013-07-18 12:04:57 +0200

    refactored mbkm w/o replacement + minor change

--- kmeans.py
+++ kmeans.py
@@ -16,7 +16,7 @@ def kmeans(X, C):
     return C
     
 
-def mini_batch_kmeans(X, C, b, t):
+def mini_batch_kmeans(X, C, b, t, replacement=True):
     """The mini-batch k-means algorithms (Sculley et al. 2007).
 
     X : data matrix
@@ -26,33 +26,11 @@ def mini_batch_kmeans(X, C, b, t):
     """
     for i in range(t):
         # Sample a mini batch:
-        X_batch = X[np.random.permutation(X.shape[0])[:b]]
-        V = np.zeros(C.shape[0])
-        idxs = np.empty(X_batch.shape[0])
-        # Assign the closest centers without update for the whole batch:
-        for j, x in enumerate(X_batch):
-            idxs[j] = np.argmin(((C - x)**2).sum(1))
-
-        # Update centers:
-        for j, x in enumerate(X_batch):
-            V[idxs[j]] += 1
-            eta = 1.0 / V[idxs[j]]
-            C[idxs[j]] = (1.0 - eta) * C[idxs[j]] + eta * x
-
-    return C
-
-
-def mini_batch_kmeans_without_replacement(X, C, b, t):
-    """The mini-batch k-means algorithms (Sculley et al. 2007).
-
-    X : data matrix
-    C : initial centers
-    b : size of the mini-batches
-    t : number of iterations
-    """
-    for i in range(t):
-        # Sample a mini batch:
-        X_batch = X[b*t:b*(t+1)]
+        if replacement:
+            X_batch = X[np.random.permutation(X.shape[0])[:b]]
+        else:
+            X_batch = X[b*t:b*(t+1)]
+            
         V = np.zeros(C.shape[0])
         idxs = np.empty(X_batch.shape[0])
         # Assign the closest centers without update for the whole batch:
@@ -93,10 +71,13 @@ if __name__ == '__main__':
 
     plt.plot(C[:,0], C[:,1], 'ro', markersize=10, label='k-means')
 
-    C_mbkm = mini_batch_kmeans(X, C_init, b=50, t=10)
+    b = 50
+    t = 10
+
+    C_mbkm = mini_batch_kmeans(X, C_init, b=b, t=t, replacement=True)
     plt.plot(C_mbkm[:,0], C_mbkm[:,1], 'go', markersize=10, label='mini-batch k-means')
 
-    C_mbkm_wr = mini_batch_kmeans_without_replacement(X, C_init, b=50, t=10)
+    C_mbkm_wr = mini_batch_kmeans(X, C_init, b=b, t=t, replacement=False)
     plt.plot(C_mbkm_wr[:,0], C_mbkm_wr[:,1], 'mo', markersize=10, label='mini-batch k-means w/o rep.')
 
     plt.legend(numpoints=1, loc='lower right')
